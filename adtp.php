fab3b920dc5f94a6d684d6ef6954565dc0347b31
EvgenyStefanenko <mixer@clarionlife.net>
2019-01-13 19:18:13 +1000

    first comm

---
+++ adtp.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Add date to title
+Description: Automatic adding date to title over month after publishing.
+Version: 1.0
+Author: Evgeny Stefanenko
+Author URI: https://forum.clarionlife.net
+
+*/
